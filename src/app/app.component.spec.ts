a7d6db2ef3ecdc5de20f42c4915de17f6a0737c8
Camila Sousa <camilamorgada7@gmail.com>
2022-11-20 19:30:01 -0300

    atualizando arquivo de teste

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,12 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projetos_empreendedores'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('projetos_empreendedores');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
